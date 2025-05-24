d381e9d78f7d788881062711882551cdf17684b0
Pratik <pkbh247@gmail.com>
2025-05-23 19:15:02 -0500

    Initial project setup with organized folder structure

---
+++ Sapients app/Sapients_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sapients_appApp.swift
+//  Sapients app
+//
+//  Created by Pratik Bhyan on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Sapients_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sapients appTests/Sapients_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sapients_appTests.swift
+//  Sapients appTests
+//
+//  Created by Pratik Bhyan on 23/05/25.
+//
+
+import Testing
+@testable import Sapients_app
+
+struct Sapients_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
